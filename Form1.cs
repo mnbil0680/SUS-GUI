858904c83184bd9504b67f5f580962f4b63f2794
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2024-12-13 01:54:19 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Color white = Color.FromArgb(255, 255, 255, 255);
+            Pen whitePen = new Pen(white);
+            whitePen.Width = 15;
+            //whitePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            whitePen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            whitePen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            //draw Horizental lines
+            e.Graphics.DrawLine(whitePen, 600, 300, 1250, 300);
+            e.Graphics.DrawLine(whitePen, 600, 460, 1250, 460);
+
+            //draw Vertical lines
+            e.Graphics.DrawLine(whitePen, 810, 140, 810, 620);
+            e.Graphics.DrawLine(whitePen, 1040, 140, 1040, 620);
+
+        }
+    }
+}
